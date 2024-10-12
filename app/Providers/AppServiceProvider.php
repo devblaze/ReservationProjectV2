1b1b1443b836f05e8b4a7df4913eb2fbbe7256fe
Nikolaos Christos Antoniadis <nikosantoniadis94@gmail.com>
2024-10-13 01:07:51 +0300

    Revert

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,9 +28,5 @@ class AppServiceProvider extends ServiceProvider
                 ];
             },
         ]);
-
-        if(config('app.env') === 'production') {
-            \URL::forceScheme('https');
-        }
     }
 }
