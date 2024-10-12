8b7ab75e25056b95ade40f7f27ff00cb4288cde7
Nikolaos Christos Antoniadis <nikosantoniadis94@gmail.com>
2024-10-13 00:55:01 +0300

    Final update for HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,5 +28,9 @@ class AppServiceProvider extends ServiceProvider
                 ];
             },
         ]);
+
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
